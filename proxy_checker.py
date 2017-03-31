14d75650f0116dde29b849751329371738390eb6
AlanBaumgartner <alanvbaumgartner@gmail.com>
2017-03-31 12:04:37 -0500

    Update Progress

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,6 +1,6 @@
 import asyncio, aiohttp, sys, threading
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 
 __author__ = 'Alan Baumgartner'
 
@@ -18,6 +18,11 @@ class App():
         self.save_entry = Entry(master, justify='center', width=17, highlightthickness=0)
         self.save_button = Button(master, text='Save', command=self.save_proxies, width=15, highlightthickness=0)
 
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
+        self.progress = ttk.Progressbar(master, style="red.Horizontal.TProgressbar", orient="horizontal", mode="determinate", length=450)
+
         self.save_entry.insert(0, 'filename.txt')
 
         self.entrybox_label.grid(row=0,column=0, padx=(10, 5), pady=(10,5))
@@ -28,11 +33,15 @@ class App():
         self.stop_button.grid(row=3, column=0, pady=(0,10))
         self.save_entry.grid(row=2, column=1, pady=(0,10))
         self.save_button.grid(row=3, column=1, pady=(0,10))
+        self.progress.grid(row=4,column=0, columnspan=2, pady=(0,10))
+        
 
         master.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         self.URL = 'http://check-host.net/ip'
         self.running = False
+        self.count = 0
+        
 
     def get_proxies(self):
         proxies = self.entrybox_box.get(0.0, END)
@@ -47,33 +56,42 @@ class App():
         with open(outputfile, "a") as a:
             a.write(proxies)
 
-    async def check_proxies(self, proxy, orginal_ip, session, sem):
-        try:
-            async with sem:
-                async with session.get(self.URL, proxy=proxy, timeout=3) as resp:
-                    response = (await resp.read()).decode()
-                    if response != orginal_ip:
-                        self.outputbox_box.insert(END, proxy+'\n')
-                        self.outputbox_box.see(END)
-        except Exception:
-            pass
+    async def check_proxies(self, proxy, orginal_ip, session, sem, lock):
+        async with sem:
+                if self.running:
+                    try:
+                        async with session.get(self.URL, proxy=proxy, timeout=3) as resp:
+                            response = (await resp.read()).decode()
+                            if response != orginal_ip:
+                                self.outputbox_box.insert(END, proxy+'\n')
+                                self.outputbox_box.see(END)
+                                
+                    except:
+                        pass
 
-    async def main(self, conns=50):
-        sem = asyncio.BoundedSemaphore(conns)
+                    finally:
+                        with await lock:
+                            self.count += 1
+                        self.progress['value'] = self.count
+
+    async def main(self):
+        sem = asyncio.BoundedSemaphore(50)
+        lock = asyncio.Lock()
         async with aiohttp.ClientSession() as session:
             async with session.get(self.URL) as resp:
                 orginal_ip = (await resp.read()).decode()
                 proxies = self.get_proxies()
-                #FIX CALLING, DOESNT STOP WITH THREAD
-                tasks = [self.check_proxies(proxy, orginal_ip, session, sem) for proxy in proxies]
-                await asyncio.gather(*tasks) 
+                self.progress['maximum'] = len(proxies)
+                tasks = [self.check_proxies(proxy, orginal_ip, session, sem, lock) for proxy in proxies]
+                await asyncio.gather(*tasks)
 
     def start(self):
         if self.running:
             self.stop()
-        elif not self.running:
-            self.running = True
-            self.startcheck()
+        self.count = 0
+        self.progress['value'] = self.count
+        self.running = True
+        self.startcheck()
 
     def stop(self):
         self.running = False
@@ -93,7 +111,7 @@ class App():
             loop.close()
     
     def on_closing(self):
-        if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+        if messagebox.askyesno("Quit", "Are you sure you want to quit?"):
             sys.exit()
 
 if __name__ == '__main__':
